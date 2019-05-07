03aa4d2def697db3a4d002a1810c5cd4135e02d1
sgalaano <sgalaano@gmail.com>
2019-05-07 09:40:44 +0300

    add environment.ts to gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:'https://api.github.com/users/',
+  apikey:'?access_token=8807a39b6b4f6bbc6f347275b9885be6e421346e',
+  repoapikey:'/repos?access_token=8807a39b6b4f6bbc6f347275b9885be6e421346e',
+
 };
