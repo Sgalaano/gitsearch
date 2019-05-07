773e06a484a4bb363cbac8af126762e852a59941
sgalaano <sgalaano@gmail.com>
2019-05-07 11:36:30 +0300

    update routing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:'https://api.github.com/users/',
-  apikey:'?access_token=8807a39b6b4f6bbc6f347275b9885be6e421346e',
-  repoapikey:'/repos?access_token=8807a39b6b4f6bbc6f347275b9885be6e421346e',
-
+ 
 };
