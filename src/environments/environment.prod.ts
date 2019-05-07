b4c963c39b497de37c98ec723e2ed62adf1c503d
sgalaano <sgalaano@gmail.com>
2019-05-07 11:41:33 +0300

    update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
- 
+  apiUrl:'https://api.github.com/users/',
+  apikey:'?access_token=8807a39b6b4f6bbc6f347275b9885be6e421346e',
+  repoapikey:'/repos?access_token=8807a39b6b4f6bbc6f347275b9885be6e421346e',
 };
